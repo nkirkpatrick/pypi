b49a373a1f0435c51de6f1ab4058c20d7f585d55
Norbert Kirkpatrick <norbert_kirkpatrick@yahoo.com>
2021-05-23 12:48:59 -0700

    Insert, Update and Querying Data via SQLAlchemy

--- pypi_org/views/home_views.py
+++ pypi_org/views/home_views.py
@@ -1,22 +1,25 @@
 import flask
-import services.package_service as package_service 
-#from infrastructure.view_modifiers import response
-#from pypi_org.infrastructure.view_modifiers import response
+
+from pypi_org.infrastructure.view_modifiers import response
+import pypi_org.services.package_service as package_service
+import pypi_org.services.user_service as user_service
 
 blueprint = flask.Blueprint('home', __name__, template_folder='templates')
 
+
 @blueprint.route('/')
-# @app.route('/')
-# @response(template_file='home/index.html')
+@response(template_file='home/index.html')
 def index():
-    test_packages = package_service.get_latest_packages()
-    #return {'packages': test_packages}
-    return flask.render_template('home/index.html', packages=test_packages)
+    return {
+        'releases': package_service.get_latest_releases(),
+        'package_count': package_service.get_package_count(),
+        'release_count': package_service.get_release_count(),
+        'user_count': user_service.get_user_count(),
+    }
 
 
 @blueprint.route('/about')
-# @app.route('/about')
-# @response(template_file='home/about.html')
+@response(template_file='home/about.html')
 def about():
-    return flask.render_template('home/about.html')
+    return {}
 
