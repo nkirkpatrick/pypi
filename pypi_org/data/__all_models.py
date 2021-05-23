0132efd905159bf8321690582e43acd25d4e5fa4
Norbert Kirkpatrick <norbert_kirkpatrick@yahoo.com>
2021-05-23 16:23:44 -0700

    Updated schema to include the auditing table

--- pypi_org/data/__all_models.py
+++ pypi_org/data/__all_models.py
@@ -17,4 +17,7 @@ import pypi_org.data.package
 import pypi_org.data.releases
 # noinspection PyUnresolvedReferences
 import pypi_org.data.users
+# noinspection PyUnresolvedReferences
+import pypi_org.data.audit
+
 
